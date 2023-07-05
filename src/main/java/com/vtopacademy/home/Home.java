34e4b28e497731f98d3f20ff9bf4067a5d1a18f5
macbook <timothyonoja1@gmail.com>
2023-07-05 15:21:44 +0100

    Add Home

---
+++ src/main/java/com/vtopacademy/home/Home.java
@@ -0,0 +1,16 @@
+package com.vtopacademy.home;
+
+
+public class Home {
+	
+	private final String welcomeMessage;
+
+	public Home(String welcomeMessage) {
+		this.welcomeMessage = welcomeMessage;
+	}
+
+	public String getWelcomeMessage() {
+		return welcomeMessage;
+	}
+	
+}
---
+++ src/main/java/com/vtopacademy/home/HomeController.java
@@ -0,0 +1,33 @@
+package com.vtopacademy.home;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import com.vtopacademy.account.AccountsController ;
+
+@Tag(name = "Accounts", description = "APIs for user registration and login")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class HomeController {
+	 
+	@GetMapping("/api") 
+	public EntityModel<Home> getUrls() {
+		
+		Home home  = new Home("Welcome to vtopacademy tutorial api. "
+			+ "Use this urls to access available resources.");
+		
+		return EntityModel.of(home, 
+			linkTo(methodOn(SchoolsController.class)
+				.getAllSchools()).withRel("schools"),
+			linkTo(methodOn(AccountsController.class)
+				.getAccountUrls()).withRel("accounts")
+			); 
+	} 
+} 
